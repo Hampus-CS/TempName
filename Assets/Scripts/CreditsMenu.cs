f37e1fec332448aff207641b8ccbafdba50d3655
Hampus CS <150423568+Hampus-CS@users.noreply.github.com>
2024-03-19 20:27:36 +0100

    Added a Main menu + TMP files
    Main menu with options and more while also installing TextMeshPro.

---
+++ Assets/Scripts/CreditsMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMenu : MonoBehaviour
+{
+    [Header("Menu Objects")]
+    public GameObject mainMenu;
+    public GameObject creditsMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return))
+        {
+            mainMenu.SetActive(true);
+            creditsMenu.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public MainMenuMusicManager musicManager;
+
+    public void Play()
+    {
+
+        if (musicManager != null)
+        {
+
+            musicManager.StopMainMenuMusic();
+            SceneManager.LoadScene(1);
+
+        }
+
+    }
+
+    public void Quit()
+    {
+
+        Debug.Log("Player has quit the game");
+        Application.Quit();
+
+    }
+
+}
